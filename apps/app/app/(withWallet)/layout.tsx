0953fd2954c8c54f6ea01522610128d7fffd1489
olgamaker <olga.maker@gmail.com>
2025-06-23 08:56:29 +0100

    Update to SSR wallet fetching

--- apps/app/app/(withWallet)/layout.tsx
+++ apps/app/app/(withWallet)/layout.tsx
@@ -1,15 +1,22 @@
 import { Header } from "@/components/layout/header";
 import { NetworkChecker } from "@/components/layout/network-checker";
 import { WalletUpdater } from "@/components/wallet/wallet";
+import { fetchWallet } from "@/lib/api/wallet";
 
-export default function WithWalletLayout({
+// SSR Wallet fetching
+export default async function WithWalletLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const wallet = await fetchWallet();
+	if (!wallet) {
+		throw new Error("Wallet not found");
+	}
+
 	return (
 		<>
-			<WalletUpdater />
+			<WalletUpdater wallet={wallet} />
 			<Header />
 
 			<main className="flex-1 max-w-3xl mx-auto w-full px-4">
